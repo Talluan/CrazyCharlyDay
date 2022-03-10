09894981729aa3e75f15840ec7c3bd759d30bcc3
Talluan <tmclaudel@gmail.com>
2022-03-10 18:08:50 +0100

    affichage cards

--- src/client/script/index.js
+++ src/client/script/index.js
@@ -1,10 +1,12 @@
 import catalogue from "./modules/catalogue.js"
 import * as product from "./modules/products.js";
+import cart from "./modules/cart.js"
 
 document.setModalContent = product.setModalContent;
 
 function init() {
     catalogue.displayNav()
+    cart.ajouterProduit(9)
 }
 
 window.onload = init;
