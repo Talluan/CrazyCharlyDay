36395df744f6f36d5a9c7d8cd6957da23891fcdb
Emalios <35004924+Emalios@users.noreply.github.com>
2022-03-10 18:02:02 +0100

    modifier, creer et supprimer catégorie.

--- src/server/routes.js
+++ src/server/routes.js
@@ -57,7 +57,7 @@ module.exports = app => {
         );
     });
     app.post('/api/categorie',(req, res) => {
-
+        Categorie.creerCategorie(req.body.nom);
     });
     app.put('/api/produit/:id', (req, res) => {
         Produit.trouverProduit(req.params.id).then(produit => {
@@ -85,28 +85,9 @@ module.exports = app => {
             req.body.idBox,
             req.body.idDestinataire,
             req.body.content);
-        // Commande.trouverCommande(req.params.id).then(commande => {
-        //     if (commande) {
-        //         commande.update({
-        //             couleur: req.body.couleur,
-        //             message: req.body.message,
-        //             idBox: req.body.idBox,
-        //             idDestinataire: req.body.idDestinataire,
-        //             content: req.body.content
-        //         });
-        //     } else {
-        //         Commande.create({
-        //             couleur: req.body.couleur,
-        //             message: req.body.message,
-        //             idBox: req.body.idBox,
-        //             idDestinataire: req.body.idDestinataire,
-        //             content: req.body.content
-        //         });
-        //     }
-        // }).catch(err => res.status(500).json(err));
     });
     app.put('/api/categorie/:id', (req, res) => {
-        
+        Categorie.modifierCategorie(req.params.id, req.body.nom);
     });
     app.delete('/api/produit/:id', (req, res) => {
         Produit.supprimerProduit(res.params.id);
@@ -115,5 +96,6 @@ module.exports = app => {
         Commande.supprimerCommande(res.params.id);
     });
     app.delete('/api/categorie/:id', (req, res) => {
+        Categorie.supprimerCategorie(req.params.id);
     });
 };
